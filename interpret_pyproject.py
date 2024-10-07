8230948a595c0331138be52a5efdb70260c21392
= <=>
2024-10-07 17:36:08 -0400

    improving interpret python

--- interpret_pyproject.py
+++ interpret_pyproject.py
@@ -32,18 +32,39 @@ parser = argparse.ArgumentParser(
     prog= "interpret pyproject.toml for Github Workflows",
     description=description
 )
-parser.add_argument("--python_versions",help="The python versions we test, all of which our code should work when using.")
+def create_value_identifier(parser:argparse.ArgumentParser, argument_name:str,description:str)-> None:
+    "creates the commandline arguments that identify the value to extract from pyproject.toml."
+    parser.add_argument("--"+argument_name,dest=argument_name,action="store_true",default=False,help=description)
+
+create_value_identifier(parser,"python_versions","The python versions we test, all of which our code should work when using.")
+create_value_identifier(parser,"homepage","The homepage URL for the repository.")
+create_value_identifier(parser,"repository","The github repository URL.")
+create_value_identifier(parser,"documentation","The documentation URL for the repository.")
 
 args = parser.parse_args()
 output = {}
 py_project_toml = "pyproject.toml"
 py_project_data = toml.load(py_project_toml)
 
-if "python_versions" in args:
-    "The versions of python that must pass all tests in github workflows"
+if args.python_versions:
+    #The versions of python that must pass all tests in github workflows
     output["python_versions"] = \
         py_project_data["tests"]["workflows"]["python-versions"]
+    
+if args.homepage:
+    #URL of repository homepage
+    output["homepage"] = \
+        py_project_data["tool"]["poetry"]["homepage"]
 
+if args.repository:
+    #URL of github repository 
+    output["repository"] = \
+        py_project_data["tool"]["poetry"]["repository"]
+    
+if args.documentation:
+    #URL of repository documentation
+    output["documentation"] = \
+        py_project_data["tool"]["poetry"]["documentation"]
 
 json_encoder = json.JSONEncoder()
 
